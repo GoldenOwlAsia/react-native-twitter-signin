2354f4fc5a8417b89409d885f10f6210d329851d
Nguyen The Thang <thethanghn@gmail.com>
2016-05-22 16:16:23 +0700

    wrapper for iOS

---
+++ ios/TwitterSignin/TwitterSignin.h
@@ -0,0 +1,13 @@
+//
+//  TwitterSignin.h
+//  TwitterSignin
+//
+//  Created by Justin Nguyen on 22/5/16.
+//  Copyright © 2016 Golden Owl. All rights reserved.
+//
+
+#import "RCTBridge.h"
+#import <Foundation/Foundation.h>
+
+@interface TwitterSignin : NSObject <RCTBridgeModule>
+@end
---
+++ ios/TwitterSignin/TwitterSignin.m
@@ -0,0 +1,61 @@
+//
+//  TwitterSignin.m
+//  TwitterSignin
+//
+//  Created by Justin Nguyen on 22/5/16.
+//  Copyright © 2016 Golden Owl. All rights reserved.
+//
+#import <Fabric/Fabric.h>
+#import <TwitterKit/TwitterKit.h>
+#import "RCTEventDispatcher.h"
+#import "TwitterSignin.h"
+
+@implementation TwitterSignin
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(logIn:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret callback:(RCTResponseSenderBlock)callback)
+{
+    [[Twitter sharedInstance] startWithConsumerKey:consumerKey consumerSecret:consumerSecret];
+    [Fabric with:@[[Twitter class]]];
+    
+    [[Twitter sharedInstance] logInWithMethods:TWTRLoginMethodWebBased completion:^(TWTRSession *session, NSError *error) {
+        if (error) {
+            NSDictionary *body = @{
+                                   @"domain":error.domain,
+                                   @"code":@(error.code),
+                                   @"userInfo":[error.userInfo description]
+                                   };
+            callback(@[body, [NSNull null]]);
+        } else {
+            TWTRAPIClient *client = [TWTRAPIClient clientWithCurrentUser];
+            NSURLRequest *request = [client URLRequestWithMethod:@"GET"
+                                                             URL:@"https://api.twitter.com/1.1/account/verify_credentials.json"
+                                                      parameters:@{@"include_email": @"true", @"skip_status": @"true"}
+                                                           error:nil];
+            [client sendTwitterRequest:request completion:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                NSError *jsonError;
+                NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                NSString *email = @"";
+                if (json[@"email"]) {
+                    email = json[@"email"];
+                }
+                NSLog(@"email here");
+                NSLog(email);
+                NSDictionary *body = @{@"authToken": session.authToken,
+                                       @"authTokenSecret": session.authTokenSecret,
+                                       @"userID":session.userID,
+                                       @"email": email,
+                                       @"userName":session.userName};
+                callback(@[[NSNull null], body]);
+            }];
+        }
+    }];
+}
+
+@end
