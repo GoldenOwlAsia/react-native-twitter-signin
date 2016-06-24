f232a2140b3e8f8d4e3836ccc7774b473229b798
Daniel Miller <dan@launchbadge.com>
2016-06-23 18:08:07 -0700

    Should be IOS changes

--- ios/TwitterSignin/TwitterSignin.m
+++ ios/TwitterSignin/TwitterSignin.m
@@ -19,11 +19,15 @@
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(logIn:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(logIn:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback)
 {
+    NSString *consumerKey = [RCTConvert NSString:options[@"consumerKey"]];
+    NSString *consumerSecret = [RCTConvert NSString:options[@"consumerSecret"]];
+    BOOL *getEmail = [RCTConvert BOOL:options[@"requestEmail"]];
+
     [[Twitter sharedInstance] startWithConsumerKey:consumerKey consumerSecret:consumerSecret];
     [Fabric with:@[[Twitter class]]];
-    
+
     [[Twitter sharedInstance] logInWithMethods:TWTRLoginMethodWebBased completion:^(TWTRSession *session, NSError *error) {
         if (error) {
             NSDictionary *body = @{
@@ -33,11 +37,17 @@ RCT_EXPORT_METHOD(logIn:(NSString *)consumerKey consumerSecret:(NSString *)consu
                                    };
             callback(@[body, [NSNull null]]);
         } else {
+            // Handle choosing if we want to fectch email
+            NSDictionary *parameters = @{@"include_email": @"false", @"skip_status": @"true"}
+            if (getEmail) {
+              *parameters = @{@"include_email": @"true", @"skip_status": @"true"}
+            }
+
             TWTRAPIClient *client = [TWTRAPIClient clientWithCurrentUser];
             NSURLRequest *request = [client URLRequestWithMethod:@"GET"
                                                              URL:@"https://api.twitter.com/1.1/account/verify_credentials.json"
-                                                      parameters:@{@"include_email": @"true", @"skip_status": @"true"}
-                                                           error:nil];
+                                                             parameters: parameters
+                                                             error:nil];
             [client sendTwitterRequest:request completion:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
                 NSError *jsonError;
                 NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
