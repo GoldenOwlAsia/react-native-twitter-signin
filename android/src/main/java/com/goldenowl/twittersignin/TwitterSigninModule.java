7c51b94d7d6a520facdc27e1dd438a13ff85ec1d
mitjacotic <mitja.cotic@qloud.io>
2016-10-17 15:27:55 +0200

    Update TwitterSigninModule.java

--- android/src/main/java/com/goldenowl/twittersignin/TwitterSigninModule.java
+++ android/src/main/java/com/goldenowl/twittersignin/TwitterSigninModule.java
@@ -84,7 +84,7 @@ public class TwitterSigninModule extends ReactContextBaseJavaModule implements A
             @Override
             public void failure(TwitterException exception) {
                 Log.d("failure", exception.toString());
-                callback.invoke(exception, null);
+                callback.invoke(exception.toString(), null);
             }
         });
     }
