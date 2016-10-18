2c68cd6189a2eef6085e6a34b64e22561274f132
mitjacotic <mitja.cotic@qloud.io>
2016-10-18 03:21:58 -0700

    Update TwitterSignin.m

--- ios/TwitterSignin/TwitterSignin.m
+++ ios/TwitterSignin/TwitterSignin.m
@@ -19,10 +19,25 @@
 
 RCT_EXPORT_MODULE();
 
+RCT_EXPORT_METHOD(logOut:(BOOL *)forceClearCookies callback:(RCTResponseSenderBlock)callback)
+{
+  
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.twitter.com"];
+    NSArray *cookies = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookiesForURL:url];
+    for (NSHTTPCookie *cookie in cookies)
+    {
+      NSLog(@"TWITTER LOGUT - clearing cookie");
+      [[NSHTTPCookieStorage sharedHTTPCookieStorage] deleteCookie:cookie];
+    }
+    
+    callback(@[[NSNull null], @"true"]);
+}
+
 RCT_EXPORT_METHOD(logIn:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret callback:(RCTResponseSenderBlock)callback)
 {
-    [[Twitter sharedInstance] startWithConsumerKey:consumerKey consumerSecret:consumerSecret];
-    [Fabric with:@[[Twitter class]]];
+    //[[Twitter sharedInstance] startWithConsumerKey:consumerKey consumerSecret:consumerSecret];
+    //[Fabric with:@[[Twitter class]]];
     
     [[Twitter sharedInstance] logInWithMethods:TWTRLoginMethodWebBased completion:^(TWTRSession *session, NSError *error) {
         if (error) {
@@ -36,7 +51,7 @@ RCT_EXPORT_METHOD(logIn:(NSString *)consumerKey consumerSecret:(NSString *)consu
             TWTRAPIClient *client = [TWTRAPIClient clientWithCurrentUser];
             NSURLRequest *request = [client URLRequestWithMethod:@"GET"
                                                              URL:@"https://api.twitter.com/1.1/account/verify_credentials.json"
-                                                      parameters:@{@"include_email": @"true", @"skip_status": @"true"}
+                                                      parameters:@{@"include_email": @"false", @"skip_status": @"true"}
                                                            error:nil];
             [client sendTwitterRequest:request completion:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
                 NSError *jsonError;
@@ -45,8 +60,8 @@ RCT_EXPORT_METHOD(logIn:(NSString *)consumerKey consumerSecret:(NSString *)consu
                 if (json[@"email"]) {
                     email = json[@"email"];
                 }
-                NSLog(@"email here");
-                NSLog(email);
+                //NSLog(@"email here");
+                //NSLog(email);
                 NSDictionary *body = @{@"authToken": session.authToken,
                                        @"authTokenSecret": session.authTokenSecret,
                                        @"userID":session.userID,
