0138c8c4e59c37fc9c36e7f11f54a89aa82261e1
Nguyen The Thang <justin.ntt@gmail.com>
2017-07-03 15:20:25 +0700

    logout

--- Example/TwitterButton.js
+++ Example/TwitterButton.js
@@ -7,6 +7,7 @@ import {
   View,
   Alert,
   NativeModules,
+  TouchableOpacity,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -19,20 +20,57 @@ const Constants = {
 };
 
 export default class TwitterButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false,
+    }
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
   _twitterSignIn() {
       RNTwitterSignIn.init(Constants.TWITTER_COMSUMER_KEY, Constants.TWITTER_CONSUMER_SECRET);
       RNTwitterSignIn.logIn()
         .then((loginData)=>{
           console.log(loginData);
+          const { authToken, authTokenSecret } = loginData;
+          if (authToken && authTokenSecret) {
+            this.setState({
+              isLoggedIn: true,
+            });
+          }
         }).catch((error)=>{
           console.log(error);
         });
   }
+
+  handleLogout() {
+    console.log('logout');
+    RNTwitterSignIn.logOut();
+    this.setState({
+      isLoggedIn: false,
+    });
+  }
+
   render() {
+    const { isLoggedIn } = this.state;
     return (
-      <Icon.Button name='logo-twitter' size={32} color='white' style={styles.icon} onPress={this._twitterSignIn.bind(this)}>
-        Login with Twitter
-      </Icon.Button>
+      <View style={{flex: 1}}>
+        {
+          isLoggedIn
+          ?
+          <TouchableOpacity
+            onPress={this.handleLogout}
+          >
+            <Text>Log out</Text>
+          </TouchableOpacity>
+          :
+          <Icon.Button name='logo-twitter' size={32} color='white' style={styles.icon} onPress={this._twitterSignIn.bind(this)}>
+            Login with Twitter
+          </Icon.Button>
+        }
+      </View>
     );
   }
 };
